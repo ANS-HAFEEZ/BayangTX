22970d8ae36e7651ffdbed546654a1862f0714dc
Ans Hafeez <ANS-HAFEEZ@users.noreply.github.com>
2021-01-29 23:58:37 +0500

    JustCompiled

--- OLED_STICK_PPM/OLED_STICK_PPM.ino
+++ OLED_STICK_PPM/OLED_STICK_PPM.ino
@@ -1,15 +1,12 @@
 #include <util/atomic.h>
 #include <EEPROM.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <EEPROM.h>
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #include "RunningAverage.h"
+#include "PPMEncoder.h"
 #include "pins.h"
-#include "iface_nrf24l01.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -30,23 +27,26 @@ static float YOffSet = 0;
 static int stick=0;
 ///////////////////////////////////////////////////////////////////////////////////////
 
-#define RF_POWER TX_POWER_5mW 
 
-// PPM stream settings
-#define CHANNELS 12 // number of channels in ppm stream, 12 ideally
+#define CHANNELS 16 // number of channels in ppm stream, 12 ideally
+#define PPM_PIN 10
 enum chan_order{
-    THROTTLE,
     ROLL,
     PITCH,
+    THROTTLE,
     YAW,
-    LED_AUX,  // (CH5)  led light, or 3 pos. rate on CX-10, H7, or inverted flight on H101
-    FLIP_AUX,  // (CH6)  flip control
-    SHOT_AUX,  // (CH7)  still camera (snapshot)
-    VID_AUX,  // (CH8)  video camera
-    HEADLESS_AUX,  // (CH9)  headless
-    RTH_AUX,  // (CH10) calibrate Y (V2x2), pitch trim (H7), RTH (Bayang, H20), 360deg flip mode (H8-3D, H22)
-    ESTOP_AUX,  // (CH11) calibrate X (V2x2), roll trim (H7), emergency stop (Bayang, Silverware)
-    BIND_AUX,  // (CH12) Reset / Rebind
+    ARM,  
+    ACRO, 
+    AUX1, 
+    AUX2,
+    AUX3,
+    AUX4,
+    AUX5,
+    AUX6,
+    AUX7,
+    AUX8,
+    AUX9,
+    AUX10
 };
 
 #define PPM_MIN 1000
@@ -55,37 +55,8 @@ enum chan_order{
 #define PPM_MAX 2000
 #define PPM_MIN_COMMAND 1300
 #define PPM_MAX_COMMAND 1700
-#define GET_FLAG(ch, mask) (PPM[ch] > PPM_MAX_COMMAND ? mask : 0)
-#define GET_FLAG_INV(ch, mask) (PPM[ch] < PPM_MIN_COMMAND ? mask : 0)
 
-// supported protocols
-enum {
-    PROTO_BAYANG = 5,
-    PROTO_BAYANG_SILVERWARE = 16
-};
-
-// EEPROM locationss
-enum{
-    ee_PROTOCOL_ID = 0,
-    ee_TXID0,
-    ee_TXID1,
-    ee_TXID2,
-    ee_TXID3
-};
-
-struct {
-    uint16_t volt1;
-    uint16_t rssi;
-    uint8_t  updated;
-    uint32_t lastUpdate;
-} telemetry_data;
-
-uint8_t transmitterID[4];
-uint8_t current_protocol;
-uint8_t packet[32];
-
-static uint16_t PPM[12] = {PPM_MIN,PPM_MIN,PPM_MIN,PPM_MIN,PPM_MID,PPM_MID,
-                           PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,};
+static uint16_t PPM[CHANNELS] = {PPM_MIN,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID};
 
 static void ReadFlash(void);
 static void WriteFlash(void);
@@ -93,22 +64,12 @@ static void ShowSticks(void);
 static void CalibSticks(void);
 
 void setup(){
-    Serial.begin(115200);
-   
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW); //start LED off
-    pinMode(MOSI_pin, OUTPUT);
-    pinMode(SCK_pin, OUTPUT);
-    pinMode(CS_pin, OUTPUT);
-    pinMode(CE_pin, OUTPUT);
-    pinMode(MISO_pin, INPUT);
-    
-    set_txid(false);
-    
-    if(!display.begin()) {
-      Serial.println(F("SSD1306 allocation failed"));
-      for(;;); // Don't proceed, loop forever
-    }
+  Serial.begin(115200);
+  ppmEncoder.begin(PPM_PIN);
+  if(!display.begin()) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
   pinMode(T_PIN,INPUT_PULLUP);
   pinMode(R_PIN,INPUT_PULLUP);
   pinMode(P_PIN,INPUT_PULLUP);
@@ -147,49 +108,9 @@ void setup(){
   }
   ReadFlash();
 }
-static bool reset=true;
-void loop(){
-  if(reset || PPM[BIND_AUX] > PPM_MAX_COMMAND) {
-    reset = false;
-    selectProtocol();
-    NRF24L01_Reset();
-    NRF24L01_Initialize();
-    Bayang_init();
-    Bayang_bind();
-  }
-  process_Bayang();
-  update_ppm();
-}
-void selectProtocol()
+
+void loop()
 {
-    uint8_t count = 10;
-
-    current_protocol = PROTO_BAYANG;   
-    if(PPM[YAW] < PPM_MIN_COMMAND)   // Rudder left
-        set_txid(true);                      // Renew Transmitter ID
-    
-     // update eeprom 
-    EEPROM.update(ee_PROTOCOL_ID, current_protocol);
-    // wait for safe throttle
-    while(PPM[THROTTLE] > PPM_SAFE_THROTTLE) {
-        delay(100);
-        update_ppm();
-    }
-}
-void set_txid(bool renew){
-  uint8_t i;
-  for(i=0; i<4; i++){
-    transmitterID[i] = EEPROM.read(ee_TXID0+i);
-  }
-  if(renew || (transmitterID[0]==0xFF && transmitterID[1]==0x0FF)) {
-    for(i=0; i<4; i++) {
-      transmitterID[i] = random() & 0xFF;
-      EEPROM.update(ee_TXID0+i, transmitterID[i]); 
-    }            
-  }
-}
-
-void update_ppm(){
 //  Serial.print("S1:");    Serial.print(digitalRead(S1_PIN));
 //  Serial.print("\tS2:");  Serial.print(digitalRead(S2_PIN));
 //  Serial.print("\tB1:");  Serial.print(digitalRead(B1_PIN));
@@ -206,8 +127,9 @@ void update_ppm(){
   PPM[ROLL]     = RRA.getFastAverage() + ROffSet;
   PPM[PITCH]    = PRA.getFastAverage() + POffSet;
   PPM[YAW]      = YRA.getFastAverage() + YOffSet;
-//
-  PPM[FLIP_AUX]    = digitalRead(S1_PIN) ? 1200 : 1800;
+
+  PPM[ARM]      = digitalRead(S1_PIN) ? 1200 : 1800;
+  PPM[ACRO]     = digitalRead(S2_PIN) ? 1200 : 1800;
 
 
 //  Serial.print("T:");      Serial.print(PPM[THROTTLE]);
@@ -216,18 +138,28 @@ void update_ppm(){
 //  Serial.print("\tY:");    Serial.print(PPM[YAW]);
 //  Serial.print("\tAUX:");  Serial.println(PPM[FLIP_AUX]);
 
-  if(bShowSticks && !bIsCalib){
+  if(bShowSticks && !bIsCalib)
+  {
     ShowSticks();
   }
-  else if(bIsCalib){
+  else if(bIsCalib)
+  {
     CalibSticks();
   }
-  else{
-    if(PPM[ROLL]  > 1485 && PPM[ROLL]  < 1515) PPM[ROLL]   = 1500;
-    if(PPM[PITCH] > 1485 && PPM[PITCH] < 1515) PPM[PITCH]  = 1500;
-    if(PPM[YAW]   > 1485 && PPM[YAW]   < 1515) PPM[YAW]    = 1500;
+  else
+  {
+    if(PPM[ROLL]  > 1465 && PPM[ROLL]  < 1495) PPM[ROLL]   = 1481;
+    if(PPM[PITCH] > 1465 && PPM[PITCH] < 1495) PPM[PITCH]  = 1481;
+    if(PPM[YAW]   > 1465 && PPM[YAW]   < 1495) PPM[YAW]    = 1481;
+    ppmEncoder.setChannel(THROTTLE, PPM[THROTTLE]);
+    ppmEncoder.setChannel(ROLL,     PPM[ROLL]);
+    ppmEncoder.setChannel(PITCH,    PPM[PITCH]);
+    ppmEncoder.setChannel(YAW,      PPM[YAW]);
+    ppmEncoder.setChannel(ARM,      PPM[ARM]);
+    ppmEncoder.setChannel(ACRO,     PPM[ACRO]);
+    ppmEncoder.setChannel(AUX1,     PPM[AUX1]);
+    ppmEncoder.setChannel(AUX2,     PPM[AUX2]);
   }
-
 }
 
 
